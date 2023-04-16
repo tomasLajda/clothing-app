f15f54675b3ecc6329ec42798e70ec0c24c66eb4
Tomas Lajda <tomas.lajda0@gmail.com>
2023-04-16 17:03:30 +0200

    Made directory component plus category item

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './categories.styles.scss';
+
+const CategoryItem = ({ id, category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div key={id} className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      ></div>
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
