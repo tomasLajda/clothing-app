8d9d7422857ec2fd94cc6c36fd67179cfab1f2a9
Tomas Lajda <tomas.lajda0@gmail.com>
2023-04-25 22:02:53 +0200

    Added sign in and edited navigation component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component.jsx';
+import SignIn from './components/sign-in/sign-in.component';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation></Navigation>}>
-        <Route index element={<Home></Home>}></Route>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -8,15 +8,18 @@ const Navigation = () => {
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrownLogo className="logo"></CrownLogo>
+          <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
+          <Link className="nav-link" to="/sign-in">
+            Sign in
+          </Link>
         </div>
       </div>
-      <Outlet></Outlet>
+      <Outlet />
     </Fragment>
   );
 };
