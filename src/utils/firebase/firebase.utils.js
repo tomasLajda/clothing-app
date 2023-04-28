2130bd97443ac89fc45383af573453ba0cc5869f
Tomas Lajda <tomas.lajda0@gmail.com>
2023-04-28 21:58:05 +0200

    firebase database sign in users

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,8 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBDfFwILlKYFgP056iigl4WQmo7UXgVgX8',
   authDomain: 'clothing-app-aa05f.firebaseapp.com',
@@ -24,3 +26,31 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot.exists());
+
+  // if user data doesn't exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('erroe creating the user', error.message);
+    }
+  }
+  // if user data exists
+  return userDocRef;
+};
