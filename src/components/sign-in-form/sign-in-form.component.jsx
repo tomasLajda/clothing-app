f08a5b1e4b214c85ec542dbf6c1420bf357e6dc3
Tomas Lajda <tomas.lajda0@gmail.com>
2023-05-02 22:01:29 +0200

    created user context and it is used in sign in

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
+
+import { UserContext } from '../../contexts/user.context';
 
 import {
   signInWithGooglePopup,
@@ -20,6 +22,8 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { email, password } = formFields;
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -33,8 +37,9 @@ const SignInForm = () => {
     event.preventDefault();
 
     try {
-      const response = await signAuthUserWithEmailAndPassword(email, password);
+      const { user } = await signAuthUserWithEmailAndPassword(email, password);
       resetFormFields();
+      setCurrentUser(user);
     } catch (error) {
       switch (error.code) {
         case 'auth/wrong-password':
