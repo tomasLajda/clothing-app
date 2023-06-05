aa3c4b2e2d1e2581ad746c874c106957d1efa22d
Tomas Lajda <tomas.lajda0@gmail.com>
2023-06-05 20:06:46 +0200

    Converting useContext to reducers

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,10 +1,8 @@
-import { Outlet } from 'react-router-dom';
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
   return (
     <div>
-      <Outlet></Outlet>
       <Directory></Directory>
     </div>
   );
