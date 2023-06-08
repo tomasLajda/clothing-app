2272746d8dd47c8a153cffff3083a9e6d5abe24d
Tomas Lajda <tomas.lajda0@gmail.com>
2023-06-09 00:12:15 +0200

    converting to redux

--- src/store/store.js
+++ src/store/store.js
@@ -7,9 +7,7 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-  Boolean
-);
+const middleWares = [logger];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -65,13 +65,15 @@ export const getCategoriesAndDocuments = async () => {
   const q = query(collectionRef);
 
   const querySnapshot = await getDocs(q);
-  const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
-    const { title, items } = docSnapshot.data();
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+  return querySnapshot.docs.map((docSnapshop) => docSnapshop.data());
 
-  return categoryMap;
+  // .reduce((acc, docSnapshot) => {
+  //   const { title, items } = docSnapshot.data();
+  //   acc[title.toLowerCase()] = items;
+  //   return acc;
+  // }, {});
+
+  // return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
